867ec9160925cb2b17330c77fb25ed5b7d362a4a
fmillox <frederic.millox@gmail.com>
2021-03-02 10:26:57 +0100

    Event controller : route add Setlist created

--- src/Controller/Api/EventController.php
+++ src/Controller/Api/EventController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Event;
 use App\Service\FanartApi;
 use App\Service\SetlistApi;
+use App\Repository\BandRepository;
 use App\Repository\UserRepository;
+use App\Repository\EventRepository;
+use App\Repository\CountryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -46,7 +51,7 @@ class EventController extends AbstractController
     }
 
     /**
-     * @Route("/api/event/{setlistId<\w+>}", name="api_event", methods={"GET"})
+     * @Route("/api/event/{setlistId<\w+>}", name="api_event_get", methods={"GET"})
      */
     public function findBySetlist(string $setlistId, SetlistApi $setlistApi, FanartApi $fanartApi): Response
     {
@@ -63,5 +68,47 @@ class EventController extends AbstractController
             'setlist' => $setlist,
             'bandImages' => $bandImages
         ], Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/api/event/{setlistId<\w+>}", name="api_event_add", methods={"POST"})
+     */
+    public function add(string $setlistId, EventRepository $eventRepository, CountryRepository $countryRepository, BandRepository $bandRepository, SetlistApi $setlistApi, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser();
+        $event = $eventRepository->findOneBy(['setlistId' => $setlistId]);
+        if ($event === null) {
+            $setlist = $setlistApi->findSetlist($setlistId);
+            if ($setlist['httpStatusCode'] === Response::HTTP_NOT_FOUND) {
+                return $this->json(['error' => 'setlistId not found'], Response::HTTP_NOT_FOUND);
+            }
+            unset($setlist['httpStatusCode']);
+
+            $country = $countryRepository->findOneBy(['countryCode' => $setlist['venue']['city']['country']['code']]);
+            if (!$country) {
+                return $this->json(['error' => 'country not found'], Response::HTTP_NOT_FOUND);
+            }
+            
+            $band = $bandRepository->findOneBy(['musicbrainzId' => $setlist['artist']['mbid']]);
+            if (!$band) {
+                return $this->json(['error' => 'band not found'], Response::HTTP_NOT_FOUND);
+            }
+
+            $event = new Event();
+            $event->setSetlistId($setlistId);
+            $event->setVenue($setlist['venue']['name']);
+            $event->setCity($setlist['venue']['city']['name']);
+            $event->setDate(\DateTime::createFromFormat('d-m-Y', $setlist['eventDate']));
+            $event->setCountry($country);
+            $event->setBand($band);
+            $entityManager->persist($event);
+
+        } elseif ($event->getUsers()->contains($user)) {
+            return $this->json('user already associated with event', Response::HTTP_FORBIDDEN);
+        }
+        $event->addUser($user);
+        $entityManager->flush();
+            
+        return $this->json(['message' => 'OK'], Response::HTTP_CREATED);
     }
 }
